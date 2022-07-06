f526b0a26d9a323207762b4e2f6fd8a7f5d9b94b
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-07-06 10:42:51 -0300

    slide-hero

--- js/scripts/modal.js
+++ js/scripts/modal.js
@@ -2,17 +2,19 @@
   const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
   const closeCard = document.querySelector('.js-close-details-pokemon');
 
-  function openDetailsPokemon() {
-    document.documentElement.classList.add('open-modal');
+  if (cardPokemon && closeCard) {
+    function openDetailsPokemon() {
+      document.documentElement.classList.add('open-modal');
+    }
+
+    function closeDetailsPokemon() {
+      document.documentElement.classList.remove('open-modal');
+    }
+
+    cardPokemon.forEach((card) => {
+      card.addEventListener('click', openDetailsPokemon);
+    });
+
+    closeCard.addEventListener('click', closeDetailsPokemon);
   }
-
-  function closeDetailsPokemon() {
-    document.documentElement.classList.remove('open-modal');
-  }
-
-  cardPokemon.forEach((card) => {
-    card.addEventListener('click', openDetailsPokemon);
-  });
-
-  closeCard.addEventListener('click', closeDetailsPokemon);
 }
---
+++ js/scripts/slide.js
@@ -0,0 +1,8 @@
+{
+  var slideHero = new Swiper('.slide-hero', {
+    effect: 'fade',
+    pagination: {
+      el: '.slide-hero .swiper-slide .main-area .area-explore .swiper-pagination',
+    },
+  });
+}
