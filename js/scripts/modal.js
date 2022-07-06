a70bb11be98ebb95c74ae324cbd0f222067766db
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-07-06 09:19:50 -0300

    header

--- js/scripts/modal.js
+++ js/scripts/modal.js
@@ -1,16 +1,18 @@
-const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
-const closeCard = document.querySelector('.js-close-details-pokemon');
+{
+  const cardPokemon = document.querySelectorAll('.js-open-details-pokemon');
+  const closeCard = document.querySelector('.js-close-details-pokemon');
 
-function openDetailsPokemon() {
-  document.documentElement.classList.add('open-modal');
+  function openDetailsPokemon() {
+    document.documentElement.classList.add('open-modal');
+  }
+
+  function closeDetailsPokemon() {
+    document.documentElement.classList.remove('open-modal');
+  }
+
+  cardPokemon.forEach((card) => {
+    card.addEventListener('click', openDetailsPokemon);
+  });
+
+  closeCard.addEventListener('click', closeDetailsPokemon);
 }
-
-function closeDetailsPokemon() {
-  document.documentElement.classList.remove('open-modal');
-}
-
-cardPokemon.forEach((card) => {
-  card.addEventListener('click', openDetailsPokemon);
-});
-
-closeCard.addEventListener('click', closeDetailsPokemon);
