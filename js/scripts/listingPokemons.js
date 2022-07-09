4f110f4d47e3413adce9d82aa21362fa16ad3725
anderson-rodriguesdev <andersonrodrigues.silva96@gmail.com>
2022-07-09 10:34:33 -0300

    dados do modal via API

--- js/scripts/listingPokemons.js
+++ js/scripts/listingPokemons.js
@@ -123,6 +123,11 @@
     const iconPokemonModal = document.getElementById('js-icon-pokemon-modal');
     const namePokemonModal = document.getElementById('js-name-pokemon-modal');
     const codePokemonModal = document.getElementById('js-code-pokemon-modal');
+    const heightPokemonModal = document.getElementById('js-height-pokemon');
+    const weightPokemonModal = document.getElementById('js-weight-pokemon');
+    const mainAbilitiesPokemonModal = document.getElementById(
+      'js-main-abilities-pokemon',
+    );
 
     imagePokemonModal.setAttribute('src', imagePokemon.getAttribute('src'));
     modalBackground.setAttribute('typePokemonModal', this.classList[2]);
@@ -133,8 +138,83 @@
     axios({
       method: 'GET',
       url: `https://pokeapi.co/api/v2/pokemon/${codePokemon}`,
-    }).then((response) => console.log(response.data));
+    }).then((response) => {
+      let data = response.data;
+
+      let infoPokemon = {
+        mainAbilities: capitalizeLetter(data.abilities[0].ability.name),
+        abilities: data.abilities,
+        types: data.types,
+        weight: data.weight,
+        height: data.height,
+        stats: data.stats,
+        urlType: data.types[0].type.url,
+      };
+
+      function getTypesPokemon() {
+        const areaTypesModal = document.getElementById('js-types-pokemon');
+
+        areaTypesModal.innerHTML = ' ';
+
+        let arrayTypes = infoPokemon.types;
+        arrayTypes.forEach((itemType) => {
+          let itemList = document.createElement('li');
+          areaTypesModal.appendChild(itemList);
+
+          let spanList = document.createElement('span');
+          spanList.classList = `tag-type ${itemType.type.name}
+          `;
+          spanList.textContent = capitalizeLetter(itemType.type.name);
+          itemList.appendChild(spanList);
+        });
+      }
+
+      function getWeaknessesPokemon() {
+        const areaWeaknesses = document.getElementById('js-weaknesses-pokemon');
+        areaWeaknesses.innerHTML = '';
+
+        axios({
+          method: 'GET',
+          url: `${infoPokemon.urlType}`,
+        }).then((response) => {
+          let weaknesses = response.data.damage_relations.double_damage_from;
+
+          weaknesses.forEach((itemWeak) => {
+            let itemListWeak = document.createElement('li');
+            areaWeaknesses.appendChild(itemListWeak);
+
+            let spanList = document.createElement('span');
+            spanList.classList = `tag-type ${itemWeak.name}`;
+            spanList.textContent = capitalizeLetter(itemWeak.name);
+            itemListWeak.appendChild(spanList);
+          });
+        });
+      }
+
+      heightPokemonModal.textContent = `${infoPokemon.height / 10}m`;
+      weightPokemonModal.textContent = `${infoPokemon.weight / 10}kg`;
+      mainAbilitiesPokemonModal.textContent = infoPokemon.mainAbilities;
+
+      const statsHp = document.getElementById('js-stats-hp');
+      const statsAttack = document.getElementById('js-stats-attack');
+      const statsDefense = document.getElementById('js-stats-defense');
+      const statsSpAttack = document.getElementById('js-stats-sp-attack');
+      const statsSpDefense = document.getElementById('js-stats-sp-defense');
+      const statsSpeed = document.getElementById('js-stats-speed');
+
+      statsHp.style.width = `${infoPokemon.stats[0].base_stat}%`;
+      statsAttack.style.width = `${infoPokemon.stats[1].base_stat}%`;
+      statsDefense.style.width = `${infoPokemon.stats[2].base_stat}%`;
+      statsSpAttack.style.width = `${infoPokemon.stats[3].base_stat}%`;
+      statsSpDefense.style.width = `${infoPokemon.stats[4].base_stat}%`;
+      statsSpeed.style.width = `${infoPokemon.stats[5].base_stat}%`;
+
+      console.log(infoPokemon.stats);
+      getTypesPokemon();
+      getWeaknessesPokemon();
+    });
   }
+
   function closeDetailsPokemon() {
     document.documentElement.classList.remove('open-modal');
   }
